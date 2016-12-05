5369dcf83e3a59bf64c7c87a1f50d0af6473bdb7
TobiasEberharter <tobias_e2@hotmail.com>
2016-12-05 21:03:57 +0100

    added a parent selection function

---
+++ selection.cpp
@@ -0,0 +1,47 @@
+#include "selection.hpp"
+#include "structures.hpp"
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <iterator>
+/* select_parents_direct
+ * 
+ * The function takes a reference to the current population
+ * and the amount of requested parents as parameter
+ * and returns, based on random tournament and fitness,
+ * a vector of individuals containing the parents
+ * 
+ * 
+ * parameters: const std::vector<individual> &population, int parents_amount
+ * return: 	   std::vector<individual> parents
+ */
+std::vector<individual> selection::select_parents_direct(const std::vector<individual> &population,int parents_amount) {
+	int tournament_size = 5;
+	int random_indiv = 0;
+	individual highest_fit;
+	std::vector<individual> tournament_population;
+	std::vector<individual> parent_population;
+	//choosing parent
+	for (int i = 0; i < parents_amount ;i++) {
+		//choosing tournament canidates
+		for (int j = 0; j < tournament_size;j++) {
+			random_indiv =  rand() % 30 ;
+			tournament_population.push_back(population.at(random_indiv));
+		}
+		//get one parent with the highest fitness
+		highest_fit = tournament_population.at(0);
+		for (int j = 1; j < tournament_size; j++) {
+			if (tournament_population.at(j).fitness > highest_fit.fitness) {
+				highest_fit = tournament_population.at(j);
+			}
+		}
+		//add the highest_fitness of the tournament to the parent vector
+		parent_population.push_back(highest_fit);
+		tournament_population.clear();
+
+		
+
+	}
+	return parent_population;
+}
