fb5e52fefc48ea4ca9e7bb4e3445016e34d388f4
daniel <daniel.allendorf@hotmail.de>
2016-12-05 19:30:26 +0100

    Improved evolutionary_algorithm.cpp to return the fittest member during the whole run, not just at the end.

--- evolutionary_algorithm.cpp
+++ evolutionary_algorithm.cpp
@@ -14,6 +14,9 @@ individual evolutionary_algorithm(KusiakLayoutEvaluator& evaluator,
                                   int generations) {   
    // intialization step
    std::vector<individual> population = initialize(evaluator, wscenario);
+
+   // this will store the maximum fittest member
+   individual fittest = *population.begin();
    
    for (int g = 0; g < generations; ++g) {
       // selection step
@@ -27,17 +30,14 @@ individual evolutionary_algorithm(KusiakLayoutEvaluator& evaluator,
       
       // replacement step
       population = replace(population, children);
-   }
-   
-   // final selection, always select the fittest member
-   auto fittest = population.begin();
-   for (auto iter = population.begin(); iter != population.end(); ++iter) {
-      if (fittest->fitness < iter->fitness) {
-         fittest = iter;
+
+      // update the fittest member
+      for (auto iter = population.begin(); iter != population.end(); ++iter) {
+	 if (fittest.fitness < iter->fitness) {
+	    fittest = *iter;
+	 }
       }
    }
-   // TODO: if population is empty, this will crash
-   // either make it a requirement that it's never empty
-   // or add an exception or something
-   return *fittest;
+   
+   return fittest;
 }
