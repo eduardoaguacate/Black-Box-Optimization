f5e9e4b4cc673b9d9ed6a8132f2d84c66f1059b5
Eduardo Serna <lalotahoma@icloud.com>
2016-12-06 14:14:24 +0100

    Made a Random class for generating random numbers using Mersenne Twister alongside the uniform in distribution.

---
+++ Random.hpp
@@ -0,0 +1,36 @@
+// Random.hpp
+//
+// This class creates random numbers using the Mersenne Twister random number
+// generator
+//
+
+#ifndef BBO_RANDOM_HPP
+#define BBO_RANDM_HPP
+
+class Random {
+  public:
+    /* Random (constructor)
+    *
+    * This is the default constructor of the random number generator
+    */
+    Random();
+
+    /* DrawNumber
+    *
+    * This function will randomly draw number from the given range, inclusive
+    * using the uniform int distribution
+    *
+    * params:
+    *       min: the lower bound
+    *       max: the upper bound
+    * return:
+    *       int : a randomly generated number from min to max
+    */
+    int DrawNumber(int min, int max);
+
+  private:
+      //The Mersenne Twister random number generator
+      std::mt19937 mt19937_eng;
+};
+
+#endif
