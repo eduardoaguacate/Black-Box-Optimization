a39325aeb66e49180677c8ac2ada882bbd5efbcf
Eduardo Serna <lalotahoma@icloud.com>
2016-12-06 14:13:27 +0100

    Commented faulty code in main.cpp .

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@
 #include "selection.hpp"
 #include "recombination.hpp"
 #include "mutation.hpp"
-#include "replacement.hpp"
+#include "replacement.cpp"
 #include "evolutionary_algorithm.hpp"
 
 int main(int argc, const char * argv[]) {
@@ -27,32 +27,32 @@ int main(int argc, const char * argv[]) {
    evaluator.initialize(wscenario);
 
    // this is just a stub, replace it once we have a real selection
-   auto selection_stub = [](std::vector<individual>& population) {
-      std::vector<std::vector<individual>::iterator> parents;
-      for (auto it = population.begin(); it != population.end(); ++it)
-      {
-         parents.push_back(it);
-      }
-      return parents;
-   };
-   
-   // this is just a stub, replace it once we have a real selection
-   auto replacement_stub = [](const std::vector<individual>& parents,
-                              const std::vector<individual>& children) {
-      return children;
-   };
+  //  auto selection_stub = [](std::vector<individual>& population) {
+  //     std::vector<std::vector<individual>::iterator> parents;
+  //     for (auto it = population.begin(); it != population.end(); ++it)
+  //     {
+  //        parents.push_back(it);
+  //     }
+  //     return parents;
+  //  };
+   //
+  //  // this is just a stub, replace it once we have a real selection
+  //  auto replacement_stub = [](const std::vector<individual>& parents,
+  //                             const std::vector<individual>& children) {
+  //     return children;
+  //  };
+   //
+  //  using namespace std::placeholders;
+  //  // this is how you use the evolutionary_algorithm
+  //  individual best = evolutionary_algorithm(
+  //     evaluator,
+  //     wscenario,
+  //     initialization::initialization_2,
+  //     selection_stub,
+  //     recombination::crossover,
+  //     std::bind(mutation::creep, 0.5, wscenario.width, wscenario.height, _1, _2),
+  //     replacement_stub,
+  //     100);
 
-   using namespace std::placeholders;
-   // this is how you use the evolutionary_algorithm
-   individual best = evolutionary_algorithm(
-      evaluator,
-      wscenario,
-      initialization::initialization_2,
-      selection_stub,
-      recombination::crossover,
-      std::bind(mutation::creep, 0.5, wscenario.width, wscenario.height, _1, _2),
-      replacement_stub,
-      100);
-   
-   std::cout << best.fitness << std::endl;
+   //std::cout << best.fitness << std::endl;
 }
