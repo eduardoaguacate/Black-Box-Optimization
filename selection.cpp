aef7dc71a1eef685885de76bbe9de735e2430348
TobiasEberharter <tobias_e2@hotmail.com>
2016-12-07 09:11:05 +0100

    selection now returns iterators, removed select_direct

--- selection.cpp
+++ selection.cpp
@@ -5,43 +5,46 @@
 #include <iterator>
 #include "selection.hpp"
 
-/* select_parents_direct
+/* selection_1
  *
  * The function takes a reference to the current population
  * and the amount of requested parents as parameter
  * and returns, based on random tournament and fitness,
- * a vector of individuals containing the parents
+ * a vector of iterators to individuals within the population
+ * containing the parents
  *
  *
- * parameters: const std::vector<individual> &population, int parents_amount
- * return: 	   std::vector<individual> parents
+ * parameters:  std::vector<individual> &population, int parents_amount
+ * return: 	   std::vector<std::vector<individual>::iterator> parent_iterators
  */
-std::vector<individual> selection::select_parents_direct(const std::vector<individual> &population,int parents_amount) {
+std::vector<std::vector<individual>::iterator> selection::selection_1(
+		 std::vector<individual> &population,int parents_amount) {
+
+
 	int tournament_size = 5;
 	int random_indiv = 0;
-	individual highest_fit;
-	std::vector<individual> tournament_population;
-	std::vector<individual> parent_population;
-	//choosing parent
-	for (int i = 0; i < parents_amount ;i++) {
-		//choosing tournament canidates
+	std::vector<individual>::iterator highest_fit;
+	std::vector<individual>::iterator candidate_iterator;
+	std::vector<std::vector<individual>::iterator> tournament_iterators;
+	std::vector<std::vector<individual>::iterator> parent_iterators;
+	//choosing parents:
+	for (int i = 0;i < parents_amount;i++) {
+		//choosing tournament candidates
 		for (int j = 0; j < tournament_size;j++) {
-			random_indiv =  rand() % 30 ;
-			tournament_population.push_back(population.at(random_indiv));
+			random_indiv = rand() % population.size();
+			candidate_iterator = population.begin() + random_indiv;
+			tournament_iterators.push_back(candidate_iterator);
 		}
-		//get one parent with the highest fitness
-		highest_fit = tournament_population.at(0);
-		for (int j = 1; j < tournament_size; j++) {
-			if (tournament_population.at(j).fitness > highest_fit.fitness) {
-				highest_fit = tournament_population.at(j);
+		// get the parent with the highest fitness out of the tournament
+		highest_fit = tournament_iterators.at(0);
+		for (unsigned int j = 1;j < tournament_iterators.size();j++) {
+			if (tournament_iterators.at(j)->fitness > (highest_fit)->fitness ) {
+				highest_fit = tournament_iterators.at(j);
 			}
 		}
-		//add the highest_fitness of the tournament to the parent vector
-		parent_population.push_back(highest_fit);
-		tournament_population.clear();
-
-
-
+		//add the highest_fit to the parent vector
+		parent_iterators.push_back(highest_fit);
+		tournament_iterators.clear();
 	}
-	return parent_population;
+	return parent_iterators;
 }
