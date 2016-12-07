7a8fb7bfb0abb8f5d99119ff8c87562aa6188f47
TobiasEberharter <tobias_e2@hotmail.com>
2016-12-07 12:33:38 +0100

    Update selection.hpp

--- selection.hpp
+++ selection.hpp
@@ -21,34 +21,5 @@ namespace selection{
 std::vector<std::vector<individual>::iterator> selection::selection_1(
 		 std::vector<individual> &population,int parents_amount) {
 
-
-	int tournament_size = 5;
-	int random_indiv = 0;
-	std::vector<individual>::iterator highest_fit;
-	std::vector<individual>::iterator candidate_iterator;
-	std::vector<std::vector<individual>::iterator> tournament_iterators;
-	std::vector<std::vector<individual>::iterator> parent_iterators;
-	//choosing parents:
-	for (int i = 0;i < parents_amount;i++) {
-		//choosing tournament candidates
-		for (int j = 0; j < tournament_size;j++) {
-			random_indiv = rand() % population.size();
-			candidate_iterator = population.begin() + random_indiv;
-			tournament_iterators.push_back(candidate_iterator);
-		}
-		// get the parent with the highest fitness out of the tournament
-		highest_fit = tournament_iterators.at(0);
-		for (unsigned int j = 1;j < tournament_iterators.size();j++) {
-			if (tournament_iterators.at(j)->fitness > (highest_fit)->fitness ) {
-				highest_fit = tournament_iterators.at(j);
-			}
-		}
-		//add the highest_fit to the parent vector
-		parent_iterators.push_back(highest_fit);
-		tournament_iterators.clear();
-	}
-	return parent_iterators;
 }
 }
-
-
