8f93c641dbe16cb1efbcbfacc5267b25c93d1998
Eduardo Serna <Eduardo@Eduardo-MBP.local>
2016-12-18 00:25:37 +0100

    Replacement is now based on pop size.

--- replacement.cpp
+++ replacement.cpp
@@ -4,8 +4,9 @@
 #include "replacement.hpp"
 
 namespace replacement {
-std::vector<individual> replacement_1(std::vector<individual> &old,
-                                      std::vector<individual> &children){
+    std::vector<individual> replacement_1(std::vector<individual> &old,
+                                          std::vector<individual> &children,
+                                          int num_population){
         // The new population
         std::vector<individual> new_population;
 
@@ -16,37 +17,50 @@ std::vector<individual> replacement_1(std::vector<individual> &old,
         // We initialize the random number generator
         Random rand;
 
-        //We select the 10 most fitted members from the parents
-        for (auto it = old.end() - 1; it > old.begin() + 19; --it) {
-                new_population.push_back(*it);
-                old.erase(it);
+        // This fraction is used in order to start on the 2/3'th part of the
+        // vector, to get the 1/3 part of it.
+        int fraction = (2.0 / 3.0) * num_population;
+        //We select the 1/3 of the most fitted members from the parents
+        for (auto it = old.end() - 1; it > old.begin() + (fraction - 1) ; --it) {
+            new_population.push_back(*it);
+            old.erase(it);
         }
 
-        //We select the 10 most fitted members from the children
-        for (auto it = children.end() - 1; it > children.begin() + 19; --it) {
-                new_population.push_back(*it);
-                children.erase(it);
+        //We select 1/3 of the most fitted members from the children
+        for (auto it = children.end() - 1; it > children.begin() + (fraction - 1); --it) {
+            new_population.push_back(*it);
+            children.erase(it);
         }
 
-        // We select five random numbers from both vectors
-        for (int i = 0; i < 5; ++i) {
-
+        // We select the 1/3 of the new population by adding it to the vector
+        // until we reach num_population
+        
+        // We use this sentinel to alternate the fill in of the new population vector
+        int i = 0;
+        
+        // We make a random mod number so that the new population isn't biased
+        // towards always having more children or more of the old population
+        int mod = rand.DrawNumber<int>(0, 1);
+        while(new_population.size() <  num_population){
+            if (!old.empty() && i % 2 == mod){
                 // We randomly get a position from the parents
                 auto it = old.begin() + rand.DrawNumber<int>(0,old.size() - 1);
                 new_population.push_back(*it);
-
                 //We erase this element so that we dont push back repeated parents
                 old.erase(it);
-
+            }
+            
+            if (!children.empty() && i % 2 == (mod - 1) ){
                 // We randomly get a position from the children
-                it = children.begin() + rand.DrawNumber<int>(0,children.size() - 1);
+                auto it = children.begin() + rand.DrawNumber<int>(0,children.size() - 1);
                 new_population.push_back(*it);
-
+                
                 //We erase this element so that we dont push back repeated children
                 children.erase(it);
-
+            }
+            i++;
         }
-
+        
         return new_population;
-}
+    }
 }
--- replacement.hpp
+++ replacement.hpp
@@ -12,24 +12,27 @@
 #include "random.hpp"
 
 namespace replacement {
-/* replacement_1
- *
- * This implementation does a survivor selection mechanism, in which the most
- * fitted members ar eselected, as well as some others randomly, to prevent
- * the local optimum problem
- *
- * params:
- *     const vector<individual> &old : a reference of the current
- *                                             population.
- *     const vector<individual> &children : a reference of the children created
- *                                          in the current iteration
- *
- * returns:
- *     vector<individual> : this is a new population created from both of the
- *                          parameters received
- */
-std::vector<individual> replacement_1(std::vector<individual> &old,
-                                      std::vector<individual> &children);
+    /* replacement_1
+     *
+     * This implementation does a survivor selection mechanism, in which the most
+     * fitted members ar eselected, as well as some others randomly, to prevent
+     * the local optimum problem
+     *
+     * params:
+     *     const vector<individual> &old : a reference of the current
+     *                                             population.
+     *     const vector<individual> &children : a reference of the children created
+     *                                          in the current iteration
+     *     int num_population : The population number, which is the number of layouts or
+     *                           individuals we are going to have per generation
+     *
+     * returns:
+     *     vector<individual> : this is a new population created from both of the
+     *                          parameters received
+     */
+    std::vector<individual> replacement_1(std::vector<individual> &old,
+                                          std::vector<individual> &children,
+                                          int num_population);
 }
 
 #endif
