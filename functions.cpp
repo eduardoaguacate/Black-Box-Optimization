30647edc9989385f4785d09b02a63c19d4aefc29
Eduardo Serna <Eduardo@Eduardo-MBP.local>
2016-12-18 00:22:40 +0100

    Turbine collides now verifies the turbine doesnt collide with an obstacle.

--- functions.cpp
+++ functions.cpp
@@ -3,26 +3,43 @@
 #include "functions.hpp"
 
 namespace functions {
-bool turbine_collides(double x, double y, double min_distance,
+bool turbine_collides(double x, double y,
+                      KusiakLayoutEvaluator &evaluator,
                       std::vector<coordinate> &layout){
-        for(auto it = layout.begin(); it < layout.end(); ++it) {
-                // If there is already a coordinate with these values, then
-                // it does collide
-                if(it->x == x && it->y == y) {
-                        return true;
-                }
-                // We check the security distance constraint
-                double dif_x = it->x - x;
-                double dif_y = it->y - y;
-                double dist = sqrt(pow(dif_x,2) + pow(dif_y,2));
-                if (dist < min_distance) {
-                        return true;
-                }
+    
+    // The minimum distance so that they are in the center
+    double min_distance = 8.0 * evaluator.scenario.R;
+    for(auto it = layout.begin(); it < layout.end(); ++it) {
+        // If there is already a coordinate with these values, then
+        // it does collide
+        if(it->x == x && it->y == y) {
+            return true;
         }
-        return false;
+        // We check the security distance constraint
+        double dif_x = it->x - x;
+        double dif_y = it->y - y;
+        double dist = sqrt(pow(dif_x,2) + pow(dif_y,2));
+        if (dist < min_distance) {
+            return true;
+        }
+        
+        // We check the obstacle constraint
+        for (int j = 0; j < evaluator.scenario.obstacles.rows; j++) {
+            Matrix<double> matObstacles = evaluator.scenario.obstacles;
+            // If somehow the position of a turbine is within the range
+            // of an obstacle, then it collides, and it's an invalid turbine
+            if (it->x > matObstacles.get(j, 0) &&
+                it->x < matObstacles.get(j, 2) &&
+                it->y > matObstacles.get(j, 1) &&
+                it->y < matObstacles.get(j, 3)) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 bool compare_fitness(const individual &indiv,const individual &indiv2) {
-        return (indiv.fitness > indiv2.fitness);
+    return (indiv.fitness > indiv2.fitness);
    }
 }
--- functions.hpp
+++ functions.hpp
@@ -8,47 +8,50 @@
 
 #include "API/Matrix.hpp"
 #include "structures.hpp"
+#include "API/KusiakLayoutEvaluator.h"
 
 namespace functions {
-/* turbine_collides
- *
- * This method checks whether a given coordinate is already used
- * params:
- *      int x : the coordinate x of the turbine
- *      int y : the coordinate y of the turbine
- *      std::vector<coordinate> &layout: the layout with all the coordinates
- *
- * returns:
- *      bool : true if it collides, false otherwise
- */
-bool turbine_collides(double x, double y, double min_distance,
-                      std::vector<coordinate> &layout);
+    /* turbine_collides
+     *
+     * This method checks whether a given coordinate is already used
+     * params:
+     *      double x : the coordinate x of the turbine
+     *      double y : the coordinate y of the turbine
+     *      KusiakLayoutEvaluator evaluator : 
+                                            the KusiakLayoutEvaluator of the api
+     *      std::vector<coordinate> &layout: the layout with all the coordinates
+     *
+     * returns:
+     *      bool : true if it collides, false otherwise
+     */
+    bool turbine_collides(double x, double y, KusiakLayoutEvaluator &evaluator,
+                          std::vector<coordinate> &layout);
 
-/* vector_to_matrix
- *
- * This method makes a matrix<T> out of a vector<coordinate>
- * params:
- *      vector<coordinate> vector: the vector to be made into a matrix
- *
- * returns:
- *      Matrix<T> : the matrix made out of the vector<coordinate>
- */
-template<typename T>
-Matrix<T> individual_to_matrix(std::vector<coordinate> &vector);
+    /* vector_to_matrix
+     *
+     * This method makes a matrix<T> out of a vector<coordinate>
+     * params:
+     *      vector<coordinate> vector: the vector to be made into a matrix
+     *
+     * returns:
+     *      Matrix<T> : the matrix made out of the vector<coordinate>
+     */
+    template<typename T>
+    Matrix<T> individual_to_matrix(std::vector<coordinate> &vector);
 
-/* compare_fitness
- *
- * This function compares the fitnesses of two individuals
- *
- * params:
- *     const individual &indiv: the first individual
- *     const individual &indiv2: the second individual to be compared
- *
- * returns:
- *     bool : true if indiv->fitness is less than indiv2->fitness2
- *            false otherwise
- */
-bool compare_fitness(const individual &indiv,const individual &indiv2);
+    /* compare_fitness
+     *
+     * This function compares the fitnesses of two individuals
+     *
+     * params:
+     *     const individual &indiv: the first individual
+     *     const individual &indiv2: the second individual to be compared
+     *
+     * returns:
+     *     bool : true if indiv->fitness is less than indiv2->fitness2
+     *            false otherwise
+     */
+    bool compare_fitness(const individual &indiv,const individual &indiv2);
 }
 
 template<typename T>
