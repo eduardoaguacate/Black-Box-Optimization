d203f47310d9a2003fa8b361a5c28a777cdfe3d0
daniel <daniel.allendorf@hotmail.de>
2016-12-19 11:23:32 +0100

    Modified crossover recombination so that it doesn't make the number of turbines converge. It now returns children which have a random number of turbines from [parent_a.size, parent_b.size()]

--- recombination.cpp
+++ recombination.cpp
@@ -35,19 +35,31 @@ namespace recombination {
          auto& layout_b = it + 1 == parents.end() ?
 	    (*(parents.begin()))->layout : (*(it + 1))->layout;
 
-         // determine a random cutoff from [0, size]
-         std::size_t size = std::min(layout_a.size(), layout_b.size());
-         std::uniform_int_distribution<std::size_t> dist_size(0, size);
-         std::size_t cutoff = dist_size(rng);
+         // determine which layout has less turbines. the cutoff will be drawn from this
+         bool a_min = layout_a.size() < layout_b.size();
+         auto& min_layout = a_min ? layout_a : layout_b;
+         auto& max_layout = a_min ? layout_b : layout_a;
+         std::size_t min_size = min_layout.size();
+         std::size_t max_size = max_layout.size();
+
+         // determine a random cutoff from [0, min_size]
+         // this is the number of turbines to copy from the first parent
+         //std::size_t size = std::min(layout_a.size(), layout_b.size());
+         std::uniform_int_distribution<std::size_t> dist_min(0, min_size);
+         std::size_t cutoff = dist_min(rng);
+         // determine a size from min_layout.size() to max_layout.size()
+         // this will be the size of the child layout
+         std::uniform_int_distribution<std::size_t> dist_max(min_size, max_size);
+         std::size_t size = dist_max(rng);
 
          individual child;
-         // copy [0, cutoff] turbines from layout a
+         // copy [0, cutoff] turbines from min_layout
          for (std::size_t i = 0; i < cutoff; ++i) {
-            child.layout.push_back(layout_a[i]);
+            child.layout.push_back(min_layout[i]);
          }
-         // copy [cutoff, size] turbines from layout b
+         // copy [cutoff, size] turbines from max_layout
          for (std::size_t i = cutoff; i < size; ++i) {
-            child.layout.push_back(layout_b[i]);
+            child.layout.push_back(max_layout[i]);
          }
 
          children.push_back(child);
